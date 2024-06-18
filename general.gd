869b650492e7a403c74f911d8e27ebea87f11ca7
Myrmica29 <tristanboureau1411@gmail.com>
2024-06-18 16:55:56 +0200

    Increase box size

--- general.gd
+++ general.gd
@@ -1,6 +1,6 @@
 extends Node
 
 
-var box_size : Vector2 = Vector2(500, 500)
+var box_size : Vector2 = Vector2(600, 600)
 var density : int = 2
-var memb_r : float = 220  # rayon
+var memb_r : float = 270  # rayon
