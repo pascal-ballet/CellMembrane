f48c7bc07ca8c9bf61acaca7b1cc197cc345e4d8
Myrmica29 <tristanboureau1411@gmail.com>
2024-06-18 14:09:35 +0200

    Apparition des molécules d'eau maintenant en quinconce

--- init.gd
+++ init.gd
@@ -6,70 +6,35 @@ var ressort_path:PackedScene = load("res://ressort.tscn")
 var nb:int = 70
 var lst_memb:Array[RigidBody2D]
 var box_size : Vector2 = Vector2(400, 400)
+@export var density : int = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
 	# Creation de la membrane
-	var r:float = 185  # rayon
+	var memb_r:float = 185  # rayon
 	for m in range(nb):
 		var a = m*2*PI / nb
-		var pos:Vector2 = Vector2(box_size.x/2 + r*cos(a) ,box_size.y/2 + r*sin(a))
+		var pos:Vector2 = Vector2(box_size.x/2 + memb_r*cos(a) ,box_size.y/2 + memb_r*sin(a))
 		# Creation d'un element de membrane
 		var memb:RigidBody2D = memb_path.instantiate()
 		memb.position = pos
 		get_tree().root.add_child.call_deferred(memb)
 		lst_memb.append(memb)
-	
-	# Creation des molécules d'eau internes
-	var R : int = 10  # R vaut 2 fois le rayon d'une molécule d'eau : l'espace entre 2 anneaux
-	var n : int = 700
-	var ring : int = 0
-	var n_on_ring : int = 1
-	while n>0:
-		var x : float = ring * R * cos(n_on_ring * 2*PI / get_n_on_ring(ring))
-		var y : float = ring * R * sin(n_on_ring * 2*PI / get_n_on_ring(ring))
-		# Creation d'une molécule d'eau
-		var mol:RigidBody2D = mol_path.instantiate()
-		mol.position = box_size/2 + Vector2(x, y)
-		get_tree().root.add_child.call_deferred(mol)
-		# Mise à jour des variables dans la boucle
-		n_on_ring -= 1
-		n -= 1
-		if n_on_ring == 0:
-			ring += 1
-			n_on_ring = get_n_on_ring(ring)
-	
-	# Creation des molécules d'eau externes
-	n = 500
-	# Positionner un cercle autour de la membrane
-	ring = 200/R
-	for i in range(get_n_on_ring(ring)):
-		var x : float = ring * R * cos(i * 2*PI / get_n_on_ring(ring))
-		var y : float = ring * R * sin(i * 2*PI / get_n_on_ring(ring))
-		# Creation d'une molécule d'eau
-		var mol:RigidBody2D = mol_path.instantiate()
-		mol.position = box_size/2 + Vector2(x, y)
-		mol._is_ext = true
-		get_tree().root.add_child.call_deferred(mol)
-		n -= 1
-	# Positionner le reste dans les 4 coins
-	var corners : Array[Vector2] = [
-		Vector2(0, 0),  # Coin haut gauche
-		Vector2(box_size.x, box_size.y),  # Coin bas droite
-		Vector2(box_size.x, 0),  # Coin haut droite
-		Vector2(0, box_size.y)  # Coin bas gauche
-	]
-	var i : int = 0 
-	while n>0:
-		i += 1
-		# Creation d'une molécule d'eau
-		var mol:RigidBody2D = mol_path.instantiate()
-		mol.position = corners[i%4]
-		mol._is_ext = true
-		get_tree().root.add_child.call_deferred(mol)
-		n -= 1
 		
+	# Creation des molécules d'eau
+	var R : int = 10 + density  # espace entre 2 billes
+	for y in range(400/R):
+		for x in range(400/R):
+			var pos_x = (R/2)*(y%2) + x * R
+			var pos_y = 0 + y * R
+			# Creation d'une molécule d'eau
+			var mol:RigidBody2D = mol_path.instantiate()
+			mol.position = Vector2(pos_x, pos_y)
+			if mol.position.distance_to(box_size/2) > memb_r:
+				mol._is_ext = true
+			get_tree().root.add_child.call_deferred(mol)
+
 
 var init:bool = false
 func _process(_delta: float) -> void:
